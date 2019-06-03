b8e928e3f87eec0b4d4cf677473a47a90ed05137
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-06-02 20:04:57 -0700

    got my on click to work, but my clearInterval still isn't working correctly. continue to troubleshoot.

--- assets/javascript/backup.js
+++ assets/javascript/backup.js
@@ -1,59 +1,61 @@
 $(document).ready(function(){
 
-
+    $("#time-remaining").hide();
+    $("#correct-text").hide();
+    $("#wrong-text").hide();
     $("#start-button").show();
     $("#start-button").on("click", startGame);
 
     var counter = 5;
     var correctAnswers = 0;
     var wrongAnswers = 0;
-    var intervalID;
+    var intervalId;
     var questionArrayIndex = 0;
     var questionArray = [
         { 
-        q: "This is by far the most popular Sierra Nevada beer and is characterized by its exceptionally full-bodied, complex character and deep amber color",
+        q: "Question 1: This is by far the most popular Sierra Nevada beer and is characterized by its exceptionally full-bodied, complex character and deep amber color",
         a: ["All Day IPA" , "Pale Ale" , "Tumbler Autumn Brown Ale" , "Ruthless Rye IPA"],
         c: "Pale Ale",
         },
     
         { 
-        q: "Sierra Nevada Brewing Co. was started in 1979 by this extremely handsome bearded man?",
+        q: "Question 2: Sierra Nevada Brewing Co. was started in 1979 by this extremely handsome bearded man?",
         a: ["Jim Koch" , "Dick Yuengling" , "Matthew Ryan Hagarty", "Ken Grossman"],
         c: "Ken Grossman",
         },
     
         { 
-        q: "This small northern California town is home to the Sierra Nevada Brewery?",
+        q: "Question 3: This small northern California town is home to the Sierra Nevada Brewery?",
         a: ["Chico" , "Redding" , "Walnut Creek" , "Orangevale"],
         c: "Chico",
         },
     
         { 
-        q: "This Sierra Nevada IPA is always released just in time for the winter holidays & is best known for its citrus & pine aromas?",
+        q: "Question 4: This Sierra Nevada IPA is always released just in time for the winter holidays & is best known for its citrus & pine aromas?",
         a: ["Hazy Little Thing" , "Kellerweis" , "Celebration" , "Snow Wit White"],
         c: "Celebration",
         },
     
         { 
-        q: "This Sierra Nevada Pilsner is only available during the summer months & is best known for its crisp, golden, malt flavor?",
+        q: "Question 5: This Sierra Nevada Pilsner is only available during the summer months & is best known for its crisp, golden, malt flavor?",
         a: ["Summerfest" , "Oktoberfest" , "Tropical Torpedo", "Northern Hemisphere Harvest"],
         c: "Summerfest",
         },
     
         { 
-        q: "This barley-wine style ale from Sierra Nevada has the highest alcohol content amongst a diverse array of Sierra Nevada beers?",
+        q: "Question 6: This barley-wine style ale from Sierra Nevada has the highest alcohol content amongst a diverse array of Sierra Nevada beers?",
         a: ["Narwhal" , "Hoptimum" , "Hop Bullet" , "Bigfoot"],
         c: "Bigfoot",
         }, 
     
         { 
-        q: "More than 1,400 breweries joined together to simultaneously brew which Sierra Nevada beer in which 100% of the proceeds went to help those affected by the recent Paradise wild fire?",
+        q: "Question 7: More than 1,400 breweries joined together to simultaneously brew which Sierra Nevada beer in which 100% of the proceeds went to help those affected by the recent Paradise wild fire?",
         a: ["Torpedo" , "Old Chico" , "Resilience", "Otra Vez"],
         c: "Resilience",
         },
     
         { 
-        q: "Each year, Sierra Nevada invites a select group of homebrewers to what event in which they compete for a chance to have their own beer distributed nationwide through Sierra Nevada?",
+        q: "Question 8: Each year, Sierra Nevada invites a select group of homebrewers to what event in which they compete for a chance to have their own beer distributed nationwide through Sierra Nevada?",
         a: ["Sierra Nevada Beer Camp" , "Sierra Nevada Summer Games" , "Sierra Nevada Distributionfest" , "Sierra Nevada Homebrewfest"],
         c: "Sierra Nevada Beer Camp",
         },
@@ -61,19 +63,23 @@ $(document).ready(function(){
     ];
     
 
-    $("#answers").on("click", ".awnser-button", function() {
+    $("#answers").on("click", ".answer-button", function() {
         var userAnswer = $(this).data("name");
         if (userAnswer === questionArray[questionArrayIndex].c) {
+            clearInterval(intervalId);
             rightAnswer();
-            $("#questions").empty();
-            $("#answers").empty();
         } 
         else
         wrongAnswer();
     })
     
     function startGame() {
+        $("#correct-text").show();
+        $("#wrong-text").show();
+        $("#time-remaining").show();
         $("#counter").html(counter);
+        $("#wrong-answer").html(wrongAnswers);
+        $("#correct-answer").html(correctAnswers);
         $("#start-button").hide();
         setInterval(decrement, 1000);
         pullQuestion();
@@ -94,7 +100,7 @@ $(document).ready(function(){
         
     
     function resetCounter() {
-        clearInterval(intervalID);
+        clearInterval(intervalId);
         counter = 5;
         $("#counter").html(counter);
     }
@@ -103,7 +109,7 @@ $(document).ready(function(){
         resetCounter();
         $("#questions").empty();
         $("#answers").empty();
-        if (questionArrayIndex < 7) {
+        if (questionArrayIndex < questionArray.length - 1) {
             $("#questions").html("<p>" + questionArray[questionArrayIndex].q + "</p>");
             for (var i = 0; i < questionArray[questionArrayIndex].a.length; i++){
                 var button = $("<button>");
@@ -113,17 +119,19 @@ $(document).ready(function(){
                 $("#answers").append(button)
             }
         } else {
-            clearInterval(intervalID);
+            clearInterval(intervalId);
             $("#correct-answers").hide();
             $("#wrong-answers").hide();
-            $("correct-answers")
+            $("#correct-text").hide();
+            $("#wrong-text").hide();
+            $("#time-remaining").hide();
             $("#answers").html("You got " + correctAnswers + " questions correct & " + wrongAnswers + " wrong.");
         }
     }
 
 
     function wrongAnswer() {
-        clearInterval(intervalID);
+        resetCounter();
         wrongAnswers++;
         $("#wrong-answers").html(wrongAnswers);
         questionArrayIndex++;
@@ -131,7 +139,7 @@ $(document).ready(function(){
     }
 
     function rightAnswer() {
-        clearInterval(intervalID);
+        resetCounter();
         correctAnswers++;
         $("#correct-answers").html(correctAnswers);
         questionArrayIndex++;
